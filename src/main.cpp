99b78f5dbe01b209cf62d6134ea215bfc65dd810
dizida <dizida1@gmail.com>
2025-01-20 11:38:12 +0100

    TP 2

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,37 @@
-#include <Arduino.h>
+/*
+  Rui Santos & Sara Santos - Random Nerd Tutorials
+  Complete project details at https://RandomNerdTutorials.com/get-change-esp32-esp8266-mac-address-arduino/
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.  
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+#ifdef ESP32
+  #include <WiFi.h>
+  #include <esp_wifi.h>
+#else
+  #include <ESP8266WiFi.h>
+#endif
 
-// put function declarations here:
-const int buttonPin = 4;  // the number of the pushbutton pin
-const int ledPin =  5;    // the number of the LED pin
+void setup(){
+  Serial.begin(115200);
 
-// variable for storing the pushbutton status 
-int buttonState = 0;
-void setup() {
-  // put your setup code here, to run once:
-
-  Serial.begin(115200);  
-  // initialize the pushbutton pin as an input
-  pinMode(buttonPin, INPUT);
-  // initialize the LED pin as an output
-  pinMode(ledPin, OUTPUT);
+  Serial.print("ESP Board MAC Address: ");
+  #ifdef ESP32
+    WiFi.mode(WIFI_STA);
+    //WiFi.STA.begin();
+    uint8_t baseMac[6];
+    esp_err_t ret = esp_wifi_get_mac(WIFI_IF_STA, baseMac);
+    if (ret == ESP_OK) {
+      Serial.printf("%02x:%02x:%02x:%02x:%02x:%02x\n",
+                    baseMac[0], baseMac[1], baseMac[2],
+                    baseMac[3], baseMac[4], baseMac[5]);
+    } else {
+      Serial.println("Failed to read MAC address");
+    }
+  #else
+    Serial.println(WiFi.macAddress());
+  #endif
 }
+ 
+void loop(){
 
-void loop() {
-  // read the state of the pushbutton value
-  buttonState = digitalRead(buttonPin);
-  Serial.println(buttonState);
-  // check if the pushbutton is pressed.
-  // if it is, the buttonState is HIGH
-  if (buttonState == HIGH) {
-    // turn LED on
-    digitalWrite(ledPin, HIGH);
-  } else {
-    // turn LED off
-    digitalWrite(ledPin, LOW);
-  }
 }
